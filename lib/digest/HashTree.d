7faf36a48b09157e12b666ebd12d3e7e3dfe61fb
Ulrik Mikaelsson <ulrik.mikaelsson@gmail.com>
2010-04-05 18:25:16 +0200

    [HashTree]Fix license-info

--- lib/digest/HashTree.d
+++ lib/digest/HashTree.d
@@ -1,16 +1,21 @@
-/*******************************************************************************
-
-        copyright:      Copyright (c) 2010 Ulrik Mikaelsson. All rights reserved
-
-        license:        TODO: decide
-
-        version:        Initial release: Mar 2010
-
-        author:         Ulrik Mikaelsson
-
-        This module implements the HashTree algorithm by Ralph Merkle
-
-*******************************************************************************/
+/****************************************************************************************
+ * This module implements the HashTree algorithm by Ralph Merkle
+ *
+ *   Copyright: Copyright (C) 2009-2010 Ulrik Mikaelsson. All rights reserved
+ *
+ *   License:
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ ******************************************************************************/
 module lib.digest.HashTree;
 
 private import tango.util.container.more.Stack;
