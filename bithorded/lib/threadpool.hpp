599a66d174ea229d10b7f27ef1037c45b7c57ff6
Ulrik Mikaelsson <ulrik.mikaelsson@gmail.com>
2012-04-09 19:57:59 +0200

    Fix Copyright-assignment in all files.

--- bithorded/lib/threadpool.hpp
+++ bithorded/lib/threadpool.hpp
@@ -1,5 +1,5 @@
 /*
-    Copyright 2012 <copyright holder> <email>
+    Copyright 2012 Ulrik Mikaelsson <ulrik.mikaelsson@gmail.com>
 
     Licensed under the Apache License, Version 2.0 (the "License");
     you may not use this file except in compliance with the License.
--- bithorded/store/linkedassetstore.cpp
+++ bithorded/store/linkedassetstore.cpp
@@ -1,5 +1,5 @@
 /*
-    Copyright 2012 <copyright holder> <email>
+    Copyright 2012 Ulrik Mikaelsson <ulrik.mikaelsson@gmail.com>
 
     Licensed under the Apache License, Version 2.0 (the "License");
     you may not use this file except in compliance with the License.
@@ -13,7 +13,6 @@
     See the License for the specific language governing permissions and
     limitations under the License.
 */
-
 
 #include "linkedassetstore.hpp"
 
