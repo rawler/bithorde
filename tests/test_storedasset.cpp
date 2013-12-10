ad27a82d0f38275645821045d05cbd6ffd999ece
Ulrik Mikaelsson <ulrik.mikaelsson@gmail.com>
2013-12-10 19:53:27 +0100

    [bithorded]Tests for basic V2 assets.

--- tests/test_storedasset.cpp
+++ tests/test_storedasset.cpp
@@ -22,10 +22,11 @@ using namespace bithorded;
 struct TestData {
 	boost::asio::io_service ioSvc;
 	GrandCentralDispatch gcd;
-	fs::path testData;
+	fs::path assets;
+
 	TestData() :
 		gcd(ioSvc, 4),
-		testData(fs::path(__FILE__).parent_path() / "data")
+		assets(fs::path(__FILE__).parent_path() / "data" / "assets")
 	{}
 };
 
@@ -33,22 +34,22 @@ struct TestData {
 
 BOOST_FIXTURE_TEST_CASE( open_partial_v1_asset, TestData )
 {
-	auto asset = cache::CachedAsset::open(gcd, testData/"v1"/".bh_meta"/"assets"/"cached_partial");
+	auto asset = cache::CachedAsset::open(gcd, assets/".bh_meta"/"assets"/"v1_cached_partial");
 	BOOST_CHECK_EQUAL(asset->hasRootHash(), false);
 	BOOST_CHECK_EQUAL(asset->can_read(asset->size()-1024, 1024), 0);
 }
 
 BOOST_FIXTURE_TEST_CASE( open_fully_cached_v1_asset, TestData )
 {
-	auto asset = cache::CachedAsset::open(gcd, testData/"v1"/".bh_meta"/"assets"/"cached");
+	auto asset = cache::CachedAsset::open(gcd, assets/".bh_meta"/"assets"/"v1_cached");
 	BOOST_CHECK_EQUAL(asset->hasRootHash(), true);
 	BOOST_CHECK_EQUAL(asset->can_read(asset->size()-1024, 1024), 1024);
 }
 
 BOOST_FIXTURE_TEST_CASE( open_v1_linked_asset, TestData )
 {
-	source::Store repo(gcd, "test", testData/"v1");
-	fs::path test_asset = testData/"v1"/".bh_meta"/"assets"/"linked";
+	source::Store repo(gcd, "test", assets);
+	fs::path test_asset = assets/".bh_meta"/"assets"/"v1_linked";
 	fs::path link_target = fs::absolute(fs::read_symlink(test_asset/"data"), test_asset);
 
 	fs::remove(link_target);
@@ -76,36 +77,41 @@ BOOST_FIXTURE_TEST_CASE( open_v1_linked_asset, TestData )
 
 BOOST_FIXTURE_TEST_CASE( open_partial_v2_asset, TestData )
 {
-// 	auto asset = cache::CachedAsset::open(gcd, assetDir/"v2"/"cached_partial");
-// 	BOOST_CHECK_EQUAL(asset->hasRootHash(), false);
+	auto asset = cache::CachedAsset::open(gcd, assets/".bh_meta"/"assets"/"v2_cached_partial");
+	BOOST_CHECK_EQUAL(asset->hasRootHash(), false);
+	BOOST_CHECK_EQUAL(asset->can_read(asset->size()-1024, 1024), 0);
 }
 
 BOOST_FIXTURE_TEST_CASE( open_fully_cached_v2_asset, TestData )
 {
-// 	auto asset = cache::CachedAsset::open(gcd, assetDir/"v2"/"cached");
-// 	BOOST_CHECK_EQUAL(asset->hasRootHash(), true);
+	auto asset = cache::CachedAsset::open(gcd, assets/".bh_meta"/"assets"/"v2_cached");
+	BOOST_CHECK_EQUAL(asset->hasRootHash(), true);
+	BOOST_CHECK_EQUAL(asset->can_read(asset->size()-1024, 1024), 1024);
 }
 
 BOOST_FIXTURE_TEST_CASE( open_v2_linked_asset, TestData )
 {
-// 	fs::path test_asset = assetDir/"v2"/"linked";
-// 	fs::path link_target; /* How the FUCK do I figure out this?! */
-//
-// 	fs::remove(link_target);
-// 	// Missing asset-data should fail
-// 	BOOST_CHECK_EXCEPTION( source::SourceAsset::open(gcd, test_asset), source::AssetError, link_is_gone );
-//
-// 	{
-// 		// Create target_file
-// 		RandomAccessFile f(link_target, RandomAccessFile::READWRITE, 130*1024);
-// 		f.close();
-// 	}
-// 	fs::last_write_time(test_asset, std::time(NULL)-10000);
-// 	// Should fail due to data newer than link
-// 	BOOST_CHECK_EXCEPTION( source::SourceAsset::open(gcd, test_asset), source::AssetError, link_is_outdated );
-//
-// 	fs::last_write_time(test_asset, std::time(NULL)+10000);
-//
-// 	auto asset = source::SourceAsset::open(gcd, test_asset); // Should now succeed with newer link than data
-// 	BOOST_CHECK_EQUAL(asset->hasRootHash(), true);
+	source::Store repo(gcd, "test", assets);
+	fs::path test_asset = assets/".bh_meta"/"assets"/"v2_linked";
+	fs::path link_target = fs::absolute(assets/"link-data", test_asset);
+
+	fs::remove(link_target);
+
+	// Missing asset-data should fail
+	BOOST_CHECK_THROW( repo.openAsset(test_asset), bsys::system_error );
+
+	{
+		// Create target_file
+		RandomAccessFile f(link_target, RandomAccessFile::READWRITE, 130*1024);
+		f.close();
+	}
+	fs::last_write_time(test_asset, std::time(NULL)-10000);
+	// Should fail due to data newer than link
+	BOOST_CHECK_EQUAL( repo.openAsset(test_asset), IAsset::Ptr() );
+
+	fs::last_write_time(test_asset, std::time(NULL)+10000);
+
+	auto asset = repo.openAsset(test_asset); // Should now succeed with newer link than data
+	BOOST_CHECK_EQUAL(asset->can_read(0, 1024), 1024);
+	BOOST_CHECK_EQUAL(asset->can_read(asset->size()-1024, 1024), 1024);
 }
