fb99c73ba983688e516bf36460e80a5ecbaebdb8
Ulrik Mikaelsson <ulrik.mikaelsson@gmail.com>
2017-01-15 01:25:52 +0100

    bithorded/main: Flush logs on every item

--- bithorded/main.cpp
+++ bithorded/main.cpp
@@ -1,26 +1,42 @@
 
 #include <boost/filesystem.hpp>
 #include <boost/log/utility/setup/common_attributes.hpp>
-#include <boost/program_options.hpp>
+#include <boost/log/core.hpp>
+#include <boost/log/sinks.hpp>
 #include <iostream>
 
-#include <crypto++/files.h>
-
 #include "buildconf.hpp"
-#include "lib/hashes.h"
 #include "server/config.hpp"
 #include "server/server.hpp"
 
 using namespace std;
 namespace asio = boost::asio;
 namespace fs = boost::filesystem;
-namespace po = boost::program_options;
+namespace sinks = boost::log::sinks;
 
 using namespace bithorded;
 
+struct noop_deleter
+{
+    void operator()(void const *) const {}
+};
 
 int main(int argc, char* argv[]) {
     boost::log::add_common_attributes();
+
+    // Create a backend and attach std log to it
+    auto backend = boost::make_shared< sinks::text_ostream_backend >();
+    backend->add_stream(
+        boost::shared_ptr< std::ostream >(&std::clog, noop_deleter()));
+
+    // Enable auto-flushing after each log record written
+    backend->auto_flush(true);
+
+    // Wrap it into the frontend and register in the core.
+    // The backend requires synchronization in the frontend.
+    typedef sinks::synchronous_sink< sinks::text_ostream_backend > sink_t;
+    boost::shared_ptr< sink_t > sink(new sink_t(backend));
+    boost::log::core::get()->add_sink(sink);
 
 	try {
 		Config cfg(argc, argv);
